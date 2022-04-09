99af5cead5d9df4bed5985a7e466badafe9bc7f4
Vladyslav <vladyslav.salenik@gmail.com>
2022-04-09 18:36:48 +0300

    almost done

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField()
+
+    def __str__(self):
+        return self.title
--- portfolio/models.py
+++ portfolio/models.py
@@ -6,3 +6,6 @@ class Project(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images')
     url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
