348542d38d2f929cf6a2488264a3a26a08fa5868
Vladyslav <vladyslav.salenik@gmail.com>
2022-04-11 13:46:55 +0300

    add pagination in Blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.all_blogs, name='all_blogs'),
+    path('', views.posts_list, name='all_blogs'),
     path('<int:blog_id>', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
+from django.core.paginator import Paginator
 
-def all_blogs(request):
-    blogs = Blog.objects.order_by('-date')
-    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+# def all_blogs(request):
+#     blogs = Blog.objects.order_by('-date')
+#     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/detail.html', {'blog': blog})
+
+
+def posts_list(request):
+    posts = Blog.objects.all().order_by('-date')
+    how_much_posts_all = posts.count()
+    how_much_posts = 2
+    paginator = Paginator(posts, how_much_posts)
+    page_number = request.GET.get('page', default=1)
+    page = paginator.get_page(page_number)
+
+    return render(request, 'blog/all_blogs.html', {'posts': page, 'pag': how_much_posts_all, 'pag_2': how_much_posts})
